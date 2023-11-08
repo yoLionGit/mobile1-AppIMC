0042453e5aea7ab44b991e0901149292a9af19a7
yoLionGit <yoshiogamer@gmail.com>
2023-11-08 16:27:37 -0300

    release version

--- app/src/main/java/com/pucpr/mobile1_appimc/MainActivity.java
+++ app/src/main/java/com/pucpr/mobile1_appimc/MainActivity.java
@@ -3,6 +3,7 @@ package com.pucpr.mobile1_appimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
         editPeso = findViewById(R.id.editPeso);
         editAltura = findViewById(R.id.editAltura);
         textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularIMC(View view) {
+
+        double peso = Double.parseDouble(editPeso.getText().toString());
+        double altura = Double.parseDouble(editAltura.getText().toString());
+
+        double imc = peso/(altura*altura);
+
+        /*IMC = PESO/ALTURAxALTURA */
+
+        if(imc >= 18.5 && imc <= 24.99) {
+
+            textResultado.setText("Peso normal, IMC = "+imc);
+
+        }else if(imc >= 25 && imc <= 29.99) {
+
+            textResultado.setText("Acima do peso, IMC = "+imc);
+
+        }else if(imc >= 30 && imc <= 34.99) {
+
+            textResultado.setText("Opa seu IMC esta alto I, IMC = " +imc);
+
+        }else if(imc >= 35 && imc <= 39.99) {
+
+            textResultado.setText("Opa seu IMC esta alto II, IMC = " +imc);
+
+        }else if(imc >= 40) {
+
+            textResultado.setText("Opa seu IMC esta alto III, IMC = " +imc);
+
+        }
+
+
+
+
+
     }
 }
